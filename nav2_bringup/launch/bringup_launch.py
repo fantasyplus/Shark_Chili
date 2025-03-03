4510c67ecf300971905d6d5455eaa96ca2988408
fantasyplus <530925206@qq.com>
2025-03-03 21:53:43 +0800

    Revert "修改bringup_launch.py为单独node启动，方便调试"
    This reverts commit abdf96d8a2d44b8ebabc5053d745045ff88aa6ee.

--- nav2_bringup/launch/bringup_launch.py
+++ nav2_bringup/launch/bringup_launch.py
@@ -41,6 +41,7 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_file = LaunchConfiguration('params_file')
     autostart = LaunchConfiguration('autostart')
+    use_composition = LaunchConfiguration('use_composition')
     use_respawn = LaunchConfiguration('use_respawn')
     # log_level = LaunchConfiguration('log_level')
 
@@ -95,9 +96,7 @@ def generate_launch_description():
 
     declare_map_yaml_cmd = DeclareLaunchArgument(
         'map',
-        default_value=os.path.join(
-            bringup_dir, 'maps', 'turtlebot3_world.yaml'),
-        description='Full path to map file to load in bringup launch')
+        description='Full path to map yaml file to load')
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
@@ -125,33 +124,54 @@ def generate_launch_description():
         'log_level', default_value='info',
         description='log level')
 
-    # Specify individual nodes without composition
-    slam_launch_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'slam_launch.py')),
-        condition=IfCondition(slam),
-        launch_arguments={'namespace': namespace,
-                          'use_sim_time': use_sim_time,
-                          'autostart': autostart,
-                          'use_respawn': use_respawn,
-                          'params_file': params_file}.items())
+    # Specify the actions
+    bringup_cmd_group = GroupAction([
+        PushRosNamespace(
+            condition=IfCondition(use_namespace),
+            namespace=namespace),
 
-    localization_launch_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'localization_launch.py')),
-        condition=IfCondition(PythonExpression(['not ', slam])),
-        launch_arguments={'namespace': namespace,
-                          'map': map_yaml_file,
-                          'use_sim_time': use_sim_time,
-                          'autostart': autostart,
-                          'params_file': params_file,
-                          'use_respawn': use_respawn}.items())
+        Node(
+            condition=IfCondition(use_composition),
+            name='nav2_container',
+            package='rclcpp_components',
+            executable='component_container_isolated',
+            parameters=[configured_params, {'autostart': autostart}],
+            arguments=['--ros-args', '--log-level', log_level],
+            remappings=remappings,
+            output='screen'),
 
-    navigation_launch_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(launch_dir, 'navigation_launch.py')),
-        launch_arguments={'namespace': namespace,
-                          'use_sim_time': use_sim_time,
-                          'autostart': autostart,
-                          'params_file': params_file,
-                          'use_respawn': use_respawn}.items())
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'slam_launch.py')),
+            condition=IfCondition(slam),
+            launch_arguments={'namespace': namespace,
+                              'use_sim_time': use_sim_time,
+                              'autostart': autostart,
+                              'use_respawn': use_respawn,
+                              'params_file': params_file}.items()),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir,
+                                                       'localization_launch.py')),
+            condition=IfCondition(PythonExpression(['not ', slam])),
+            launch_arguments={'namespace': namespace,
+                              'map': map_yaml_file,
+                              'use_sim_time': use_sim_time,
+                              'autostart': autostart,
+                              'params_file': params_file,
+                              'use_composition': use_composition,
+                              'use_respawn': use_respawn,
+                              'container_name': 'nav2_container'}.items()),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(launch_dir, 'navigation_launch.py')),
+            launch_arguments={'namespace': namespace,
+                              'use_sim_time': use_sim_time,
+                              'autostart': autostart,
+                              'params_file': params_file,
+                              'use_composition': use_composition,
+                              'use_respawn': use_respawn,
+                              'container_name': 'nav2_container'}.items()),
+    ])
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -167,12 +187,11 @@ def generate_launch_description():
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_params_file_cmd)
     ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_use_composition_cmd)
     ld.add_action(declare_use_respawn_cmd)
     ld.add_action(declare_log_level_cmd)
 
-    # Add the actions for each individual node
-    ld.add_action(slam_launch_node)
-    ld.add_action(localization_launch_node)
-    ld.add_action(navigation_launch_node)
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(bringup_cmd_group)
 
     return ld
